0a83c4a9c66627af1ed999ea885a8d503a841422
johnjcaulfieldiv <johnjcaulfieldiv@gmail.com>
2020-02-10 13:33:07 -0800

    Initial commit. Fresh Project

---
+++ app/src/main/java/com/caulfield/john/blackjack/MainActivity.java
@@ -0,0 +1,14 @@
+package com.caulfield.john.blackjack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
