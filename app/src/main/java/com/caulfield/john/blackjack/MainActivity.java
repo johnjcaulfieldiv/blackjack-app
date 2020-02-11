bfcb551a7c5c4d038d4e7dae7c15299bb8690829
johnjcaulfieldiv <johnjcaulfieldiv@gmail.com>
2020-02-10 16:17:16 -0800

    renamed resource images. main menu done

--- app/src/main/java/com/caulfield/john/blackjack/MainActivity.java
+++ app/src/main/java/com/caulfield/john/blackjack/MainActivity.java
@@ -3,6 +3,8 @@ package com.caulfield.john.blackjack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* called when 'Play' button is pressed */
+    public void playGame(View view) {
+        Toast.makeText(this.getApplicationContext(), "Starting game...", Toast.LENGTH_SHORT).show();
     }
 }
